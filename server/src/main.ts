3e075b5752dc58a651bf13b0519c5f4c156fc144
Andres-J-Correa <andresj.correas@gmail.com>
2025-11-24 20:44:04 +0800

    serving privacy policy in server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsoleLogger } from '@nestjs/common';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +11,11 @@ async function bootstrap() {
       timestamp: true,
     }),
   });
+
+  app.use('/',
+    express.static(join(__dirname, '..', 'public'))
+  );
+
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
